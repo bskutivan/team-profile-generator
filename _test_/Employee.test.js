f67de174b4f9555f57c490065fc18b89efdd981c
BrandonKutivan <bskutivan@gmail.com>
2020-05-14 18:22:39 -0700

    added second condition to employee failing test. Found passing solution

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -1,9 +1,11 @@
 
 const Employee = require('../lib/Employee');
 
+
 test('create an employee object', () => {
     
     const employee = new Employee('Dave');
 
     expect(employee.name).toBe('Dave');
+    expect(employee.id).toEqual(expect.any(Number));
 });
