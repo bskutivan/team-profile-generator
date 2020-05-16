11219a280b1bf8df30452afc392fe2064961b09c
BrandonKutivan <bskutivan@gmail.com>
2020-05-16 13:58:57 -0700

    finished setting up loop for creating more employees and getting specific data for each.

--- index.js
+++ index.js
@@ -65,9 +65,97 @@ function initializePrompt() {
 const employeesPrompt = teamData => {
     if (!teamData.employees) {
         teamData.employees = [];
-        console.log(teamData.employees);
     }
+    return inquirer.prompt([
+        {
+            type:'list',
+            name: 'role',
+            message: 'What is their role?',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type:'input',
+            name:'name',
+            message: 'What is their name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter their name.');
+                    return false;
+                }
+            }
+        },
+        {
+            type:'input',
+            name:'id',
+            message:'What is their employee id?',
+            validate: idInput => {
+                if (idInput > 0) {
+                    return true;
+                } else {
+                    console.log('Please enter their employee number.');
+                    return false;
+                }
+            }
+        },
+        {
+            type:'input',
+            name:'email',
+            message:'What is their email?',
+            validate: emailInput => {
+                if (emailInput.includes("@") && emailInput.includes(".com")) {
+                    return true;
+                } else {
+                    console.log("Please input their email. It must include '@' and '.com'.");
+                    return false;
+                }
+            }
+        },
+        {
+            type:'input',
+            name:'github',
+            message: 'What is their github username?',
+            when: ({ role }) => {
+                if (role != "Engineer") {
+                    return false;
+                } else {
+                    return true;
+                }
+            } 
+                
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school are they attending?',
+            when: ({ role }) => {
+                if (role != 'Intern') {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmLoop',
+            message: 'Would you like to add anyone else?',
+            default: false
+        }
+    ])
+    .then(employeeData => {
+        teamData.employees.push(employeeData);
+        if (employeeData.confirmLoop) {
+            return employeesPrompt(teamData);
+        } else {
+            return teamData;
+        }
+    });
 }
 
 initializePrompt()
     .then(employeesPrompt)
+    .then(teamData => {
+        console.log(teamData);
+    })
