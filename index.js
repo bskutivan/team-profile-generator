b9f1aee01ae12ae898061c7badbf1e722e84a52e
BrandonKutivan <bskutivan@gmail.com>
2020-05-16 16:38:37 -0700

    took promise from newEmployeePrompt function and used conditional statements to determine which function to call next.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require("inquirer")
 const Manager = require('./lib/Manager')
 
+// array to collect team members as they are generated throughout prompting
+
+const team = [];
 
 // prompts broken up into two functions with logic for full app below prompt functions
 
@@ -66,100 +69,48 @@ function initializePrompt() {
     ]).then((answers) => {
         const {name, id, email, officeNumber} = answers
         const manager = new Manager(name, id, email, officeNumber);
-
         
-        console.log(manager);
+        // push the new manager object to team array for card generation
+        team.push(manager);
+
+        // initiate next set of inquirer prompts that determine what role of employee is being given
+        newEmployeesPrompt();
     })
 };
 
+// Asks what the next employee does and directs prompts towards correct function containing specified questions.
+
 function newEmployeesPrompt() {
     
-    return inquirer.prompt([
+    inquirer.prompt([
         {
             type:'list',
             name: 'role',
-            message: 'What is their role?',
-            choices: ['Engineer', 'Intern']
-        },
-        {
-            type:'input',
-            name:'name',
-            message: 'What is their name?',
-            validate: nameInput => {
-                if (nameInput) {
+            message: 'Are there others on the team? If so choose their roles.',
+            choices: ['Engineer', 'Intern', 'Nobody left. Build the team!'],
+            validate: roleSelection => {
+                if (roleSelection) {
                     return true;
                 } else {
-                    console.log('Please enter their name.');
                     return false;
                 }
             }
-        },
-        {
-            type:'input',
-            name:'id',
-            message:'What is their employee id?',
-            validate: idInput => {
-                if (idInput > 0) {
-                    return true;
-                } else {
-                    console.log('Please enter their employee number.');
-                    return false;
-                }
-            }
-        },
-        {
-            type:'input',
-            name:'email',
-            message:'What is their email?',
-            validate: emailInput => {
-                if (emailInput.includes("@") && emailInput.includes(".com")) {
-                    return true;
-                } else {
-                    console.log("Please input their email. It must include '@' and '.com'.");
-                    return false;
-                }
-            }
-        },
-        {
-            type:'input',
-            name:'github',
-            message: 'What is their github username?',
-            when: ({ role }) => {
-                if (role != "Engineer") {
-                    return false;
-                } else {
-                    return true;
-                }
-            } 
-                
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: 'What school are they attending?',
-            when: ({ role }) => {
-                if (role != 'Intern') {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'confirmLoop',
-            message: 'Would you like to add anyone else?',
-            default: false
         }
-    ])
-    .then(employeeData => {
-        teamData.employees.push(employeeData);
-        if (employeeData.confirmLoop) {
-            return employeesPrompt(teamData);
-        } else {
-            return teamData;
+    ]).then((answers) => {
+        const choice = answers.role;
+        console.log(choice);
+        if (choice === "Engineer") {
+            // prompt for Engineer
+            console.log('you chose engineer');
+        } else if (choice === "Intern") {
+            // prompt for Intern
+            console.log('you chose intern');
+        } else if (choice === "Nobody left. Build the team!") {
+            // write the file
+            console.log('you chose to write that file!');
         }
-    });
+
+    })
 }
 
 // logic execution starts here
