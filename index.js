f259d2abcaeb0885dea4ae2db5dc13d18688c80e
BrandonKutivan <bskutivan@gmail.com>
2020-05-16 12:15:53 -0700

    added next prompt function to get engineer and intern input

--- index.js
+++ index.js
@@ -1,30 +1,73 @@
 const inquirer = require("inquirer")
 
 function initializePrompt() {
+
+    console.log (`
+    > lets build the team!
+    `);
     
-    inquirer.prompt([
+    return inquirer.prompt([
         {
             type:'input',
             name:'name',
-            message: 'Who is the manager of this team?'
+            message: 'Who is the manager of this team?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the managers name.');
+                    return false;
+                }
+            }
         },
         {
             type:'input',
             name:'id',
-            message:'What is their employee id?'
+            message:'What is their employee id?',
+            validate: idInput => {
+                if (idInput > 0) {
+                    return true;
+                } else {
+                    console.log('Please enter their employee number.');
+                    return false;
+                }
+            }
         },
         {
             type:'input',
             name:'email',
-            message:'What is their email?'
+            message:'What is their email?',
+            validate: emailInput => {
+                if (emailInput.includes("@") && emailInput.includes(".com")) {
+                    return true;
+                } else {
+                    console.log("Please input their email. It must include '@' and '.com'.");
+                    return false;
+                }
+            }
         },
         {
             type:'input',
             name:'officeNumber',
-            message:'What is their office number?'
+            message:'What is their office number?',
+            validate: officeNumberInput => {
+                if (officeNumberInput > 0) {
+                    return true;
+                } else {
+                    console.log('Please input their office number.');
+                    return false;
+                }
+            }
         }
     ])
-
 };
 
+const employeesPrompt = teamData => {
+    if (!teamData.employees) {
+        teamData.employees = [];
+        console.log(teamData.employees);
+    }
+}
+
 initializePrompt()
+    .then(employeesPrompt)
