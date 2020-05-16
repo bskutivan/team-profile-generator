e183c0078012bec6b1c9579523f12a9211ac1442
BrandonKutivan <bskutivan@gmail.com>
2020-05-16 14:43:05 -0700

    fleshed out intern class along same code as previous classes

---
+++ _test_/Intern.test.js
@@ -0,0 +1,16 @@
+const Intern = require('../lib/Intern');
+
+test('creates an intern object', () => {
+    const intern = new Intern('Dave', 1, 'dave@gmail.com');
+
+    expect(intern.name).toBe('Dave');
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toEqual(expect.stringContaining('@' && '.com'));
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+test('getRole is overriden to return intern', () => {
+    const intern = new Intern('Dave', 1, 'dave@gmail.com');
+
+    expect(intern.getRole()).toBe('Intern');
+});
