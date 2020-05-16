f2d6e7d6b839e0042742c754145c7260f02e21f2
BrandonKutivan <bskutivan@gmail.com>
2020-05-16 14:21:34 -0700

    built out employee class and suite of tests for class

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -4,8 +4,27 @@ const Employee = require('../lib/Employee');
 
 test('create an employee object', () => {
     
-    const employee = new Employee('Dave');
+    const employee = new Employee('Dave', 1, 'test@gmail.com');
 
     expect(employee.name).toBe('Dave');
     expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.stringContaining('@' && '.com'));
 });
+
+test('getName method returns the name', () => {
+    const employee = new Employee('Dave', 1, 'test@gmail.com');
+
+    expect(employee.getName()).toBe(employee.name);
+});
+
+test('getEmail method returns the email', () => {
+    const employee = new Employee('Dave', 1, 'test@gmail.com');
+
+    expect(employee.getEmail()).toBe(employee.email);
+});
+
+test('getRole method returns the role', () => {
+    const employee = new Employee('Dave', 1, 'test@gmail.com');
+
+    expect(employee.getRole()).toBe(employee.role);
+})
